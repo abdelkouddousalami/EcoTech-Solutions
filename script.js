bd820024ff5de05faa72bf426a726c4c19ffe408
abd elkouddous <abdoalami.ru@gmail.com>
2024-10-18 15:08:18 +0100

    dark mode

--- script.js
+++ script.js
@@ -5,3 +5,7 @@ function hideShow() {
     mapSection.style.display = mapSection.style.display === 'block' ? 'none' : 'block';
     footer.style.display = footer.style.display === 'block' ? 'none' : 'block';
 }
+ // dark mode function()
+ function test() {
+    document.body.classList.toggle("dark-mode");
+}
